4c449b6a613dc40d8f066a6987a2be1d76cdc2b0
Ponnuvel Palaniyappan <pponnuvel@gmail.com>
2020-04-09 16:52:51 +0100

    exfat utils

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt install git gdb g++ gcc-multilib screen binuitls coreutils elfutils gcc openssh-server
+sudo apt install git gdb g++ gcc-multilib screen binutils coreutils elfutils gcc openssh-server exfat-fuse exfat-utils
